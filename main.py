dc08636238761d2164ca8fcda484cee91dcef1f5
Sigrid <sigridstorck@yahoo.se>
2020-12-10 18:59:48 +0100

    run game on loop

--- main.py
+++ main.py
@@ -2,7 +2,30 @@ import pygame
 
 pygame.init()
 
-# GAME CODE
+width = 800
+height = 800
+color = (204, 153, 255)
+game_window = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
 
+def run_game_loop():
+    while True:
+
+        # handle event
+        events = pygame.event.get()
+
+        for event in events:
+            if event.type == pygame.QUIT:
+                return
+
+
+        # execute logic 
+        # update display
+        game_window.fill(color)
+        pygame.display.update()
+
+        clock.tick(60)
+
+run_game_loop()
 pygame.quit()
 quit()
